08732b625f6c5af995b90787124f834ef5ad964d
Rafael Signorini <rafael_signorini@estudante.sesisenai.org.br>
2023-10-06 07:40:13 -0300

    Apaguei arquivos de Flutter que causavam incompatibilidade com outros arquivos, impossibilitando a funcionalidade de alguns arquivos. A origem e o motivo da incompatibilidade não foi descoberta.

--- Evandro/combustivel/combustivel/android/app/src/main/kotlin/com/example/combustivel/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.combustivel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Evandro/frase_do_dia/android/app/src/main/kotlin/com/example/frasedodia/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.frasedodia
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
