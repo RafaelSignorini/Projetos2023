f896c62ee32b6be27de498918df4ebaa66f9d12d
Rafael Signorini <rafael_signorini@estudante.sesisenai.org.br>
2023-10-06 09:35:23 -0300

    Criei o arquivo teste.cs e comecei a cursar C# autonomamente.

---
+++ Outros/CSharp/teste.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehavior
+{
+    int speed;
+    string enemyName;
+
+    void Movement()
+    {
+        if(speed >= 0)
+        {
+
+        }
+    }
+
+    void Attack()
+    {
+        
+    }
+}
