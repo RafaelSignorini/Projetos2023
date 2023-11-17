6ec2e98452142ddfb29ac82818792edb95cd0826
Rafael Signorini <rafael_signorini@estudante.sesisenai.org.br>
2023-11-17 07:54:03 -0300

    exercício 1

--- Outros/CSharp/cursoMicrosoft.cs
+++ Outros/CSharp/cursoMicrosoft.cs
@@ -2,3 +2,7 @@
 
 //Unit 1/8
 // Exercise - Perform addition with implicit data conversion - Add two numeric values
+
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
