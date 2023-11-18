1e6f4b729331c23d1b1a07e028767f620ce5ce1f
RafaelSignorini <rafabs26126@gmail.com>
2023-11-18 11:12:22 -0300

    Update cursoMicrosoft.cs

--- CSharp/cursoMicrosoft.cs
+++ CSharp/cursoMicrosoft.cs
@@ -1,30 +1,47 @@
 // Begginer C# exercises from Microsoft C# documentation
 
-decimal sophia1 = 93m;
-decimal sophia2 = 87m;
-decimal sophia3 = 98m;
-decimal sophia4 = 95m;
-decimal sophia5 = 100m;
+int currentAssignments = 5;
 
-decimal nicolas1 = 80m;
-decimal nicolas2 = 83m;
-decimal nicolas3 = 82m;
-decimal nicolas4 = 88m;
-decimal nicolas5 = 85m;
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
 
-decimal zahirah1 = 84m;
-decimal zahirah2 = 96m;
-decimal zahirah3 = 73m;
-decimal zahirah4 = 85m;
-decimal zahirah5 = 79m;
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
 
-decimal jeong1 = 90m;
-decimal jeong2 = 92m;
-decimal jeong3 = 98m;
-decimal jeong4 = 100m;
-decimal jeong5 = 97m;
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
 
-Console.WriteLine("Sophia's average grade was: " + (sophia1 + sophia2 + sophia3 + sophia4 + sophia5) / 5);
-Console.WriteLine("Nicolas's average grade was: " + (nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5) / 5);
-Console.WriteLine("Zahirah's average grade was: " + (zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5) / 5);
-Console.WriteLine("Jeong's average grade was: " + (jeong1 + jeong2 + jeong3 + jeong4 + jeong5) / 5);
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
+
+decimal sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+decimal nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+decimal zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+decimal jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
+
+Console.WriteLine("Sophia: " + sophiaSum);
+Console.WriteLine("Nicolas: " + nicolasSum);
+Console.WriteLine("Zahirah: " + zahirahSum);
+Console.WriteLine("Jeong: " + jeongSum);
+
+decimal sophiaScore = sophiaSum / currentAssignments;
+decimal nicolasScore = nicolasSum / currentAssignments;
+decimal zahirahScore = zahirahSum / currentAssignments;
+decimal jeongScore = jeongSum / currentAssignments;
+
+Console.WriteLine("Sophia " + sophiaScore);
+Console.WriteLine("NIcolas: " + nicolasScore);
+Console.WriteLine("Zahirah: " + zahirahScore);
+Console.WriteLine("Jeong: " + jeongScore);
