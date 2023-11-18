e6a2c52c94d01c2433a2189d9aaef00052ff5f7c
RafaelSignorini <rafabs26126@gmail.com>
2023-11-18 10:46:00 -0300

    .

---
+++ CSharp/cursoMicrosoft.cs
@@ -0,0 +1,8 @@
+// Perform basic operations on numbers on C#
+
+// Unit 1/8
+// Exercise - Perform addition with implicit data conversion - Add two numeric values
+
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
---
+++ CSharp/teste.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyAI : MonoBehavior
+{
+    float health;
+    int speed;
+    string enemyName;
+    int[] intArray = new int[] {
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    };
+    List<int> intList = new List<int>() {
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    };
+    void Movement()
+    {
+        foreach (int intNumbers in intList) { }
+        for (int i = 0; i > intArray.Length; i++) { }
+        for (int i = 0; i > intList.Count; i++) { }
+        if (speed >= 0) { } else { }
+    }
+    void Dead()
+    {
+        if (health <= 0)}
+    void Attack() { }
+}
