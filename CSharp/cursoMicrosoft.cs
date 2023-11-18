23a34906fcadd95d09c2c5edd430cfbcac71ecdf
RafaelSignorini <rafabs26126@gmail.com>
2023-11-18 10:49:14 -0300

    Update cursoMicrosoft.cs

--- CSharp/cursoMicrosoft.cs
+++ CSharp/cursoMicrosoft.cs
@@ -1,8 +1,28 @@
-// Perform basic operations on numbers on C#
+decimal sophia1 = 93m;
+decimal sophia2 = 87m;
+decimal sophia3 = 98m;
+decimal sophia4 = 95m;
+decimal sophia5 = 100m;
 
-// Unit 1/8
-// Exercise - Perform addition with implicit data conversion - Add two numeric values
+decimal nicolas1 = 80m;
+decimal nicolas2 = 83m;
+decimal nicolas3 = 82m;
+decimal nicolas4 = 88m;
+decimal nicolas5 = 85m;
 
-int firstNumber = 12;
-int secondNumber = 7;
-Console.WriteLine(firstNumber + secondNumber);
+decimal zahirah1 = 84m;
+decimal zahirah2 = 96m;
+decimal zahirah3 = 73m;
+decimal zahirah4 = 85m;
+decimal zahirah5 = 79m;
+
+decimal jeong1 = 90m;
+decimal jeong2 = 92m;
+decimal jeong3 = 98m;
+decimal jeong4 = 100m;
+decimal jeong5 = 97m;
+
+Console.WriteLine("Sophia's average grade was: " + (sophia1 + sophia2 + sophia3 + sophia4 + sophia5) / 5);
+Console.WriteLine("Nicolas's average grade was: " + (nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5) / 5);
+Console.WriteLine("Zahirah's average grade was: " + (zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5) / 5);
+Console.WriteLine("Jeong's average grade was: " + (jeong1 + jeong2 + jeong3 + jeong4 + jeong5) / 5);
