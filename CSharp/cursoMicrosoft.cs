984edc163e144fe8cb09adfd825e112566af33fa
RafaelSignorini <rafabs26126@gmail.com>
2023-11-18 23:04:30 -0300

    Update cursoMicrosoft.cs

--- CSharp/cursoMicrosoft.cs
+++ CSharp/cursoMicrosoft.cs
@@ -89,10 +89,13 @@ int leadingDigit = (int) gradePointAverage;
 int firstDigit = (int) (gradePointAverage * 10) % 10;
 int secondDigit = (int) (gradePointAverage * 100) % 10;
 
-Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
-Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
-Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
-Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
-Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
+Console.WriteLine($"Student: {studentName}\n");
+Console.WriteLine("Course\t\t\t\tGrade\tCredit Hours");
 
-Console.WriteLine($"Final GPA: {leadingDigit}.{firstDigit}{secondDigit}");
+Console.WriteLine($"{course1Name}\t\t\t{course1Grade}\t\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t{course4Grade}\t\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
+
+Console.WriteLine($"\nFinal GPA:\t\t\t {leadingDigit}.{firstDigit}{secondDigit}");
