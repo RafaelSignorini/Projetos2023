6ca8b2f215a9247d6ae72cef86b2a0ed94220e75
RafaelSignorini <rafabs26126@gmail.com>
2023-11-25 11:34:12 -0300

    Update a.py

--- Rabinson/Python/a.py
+++ Rabinson/Python/a.py
@@ -6,16 +6,16 @@ if usuario == "cinivius" and senha == "123456789":
 else:
   print("Usuário ou senha inválidos, tente novamente.")
 
-nota1 = float(input("Insira a nota da sua primeira prova aqui: "))
-if (nota1 >= 0 and nota1 <= 10):
-  pass
-else:
-  print('notpass')
-  quit()
-nota2 = float(input("Insira a nota da sua segunda prova aqui: "))
-nota3 = float(input("Insira a nota da sua terceira prova aqui: "))
-media = ((nota1 + nota2 + nota3)/3)
-if (nota1 + nota2 + nota3) /3 >= 7:
-  print(f"Sua média foi {media}, você foi aprovado.")
-else:
-  print(f"Sua média foi {media}, você foi reprovado.")
+# nota1 = float(input("Insira a nota da sua primeira prova aqui: "))
+# if (nota1 >= 0 and nota1 <= 10):
+#   pass
+# else:
+#   print('notpass')
+#   quit()
+# nota2 = float(input("Insira a nota da sua segunda prova aqui: "))
+# nota3 = float(input("Insira a nota da sua terceira prova aqui: "))
+# media = ((nota1 + nota2 + nota3)/3)
+# if (nota1 + nota2 + nota3) /3 >= 7:
+#   print(f"Sua média foi {media}, você foi aprovado.")
+# else:
+#   print(f"Sua média foi {media}, você foi reprovado.")
